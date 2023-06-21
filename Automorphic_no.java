273d63e8a467db709f92dda9401f4415bfc987b4
Pradumn Patel <pradumn.patel3101@gmail.com>
2023-06-21 21:27:02 +0530

    Create Automorphic_no.java

---
+++ Automorphic_no.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ 
+public class Automorphic {
+ 
+    public static void main(String args[]){
+ 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = in.nextInt();
+ 
+        int c=0, sqr = num*num;
+        int temp =num;  
+ 
+    
+        while(temp>0){
+            c++;
+            temp=temp/10;
+        }
+        //lSD= Last Square Digit
+        int lSD = (int) (sqr%(Math.pow(10,c)));
+ 
+        if(num == lSD)
+            System.out.println("Automorphic number");
+        else
+            System.out.println("Not an Automorphic number");
+    }
+}
