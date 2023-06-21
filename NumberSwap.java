7956495ecf6d098018528a81c24f2f2c3e026841
Pradumn Patel <pradumn.patel3101@gmail.com>
2023-06-21 21:28:14 +0530

    1st Day

---
+++ NumberSwap.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class swap{
+    public static int Swap(int number_to_swap){
+        System.out.println("The Number Given by user is :"+number_to_swap);
+        int ans=0;
+        int i=0;
+        while (number_to_swap>0){
+            if (number_to_swap>10000){
+                System.out.println("Error: Number exceded default value set.");
+            
+            }
+            else{
+                int m = number_to_swap%10;
+                ans = ans+(9-m)*(int)Math.pow(10,i++);
+                number_to_swap=number_to_swap/10;
+            }
+        }
+        return ans;
+    }
+    public static void main(String args[]) {
+        System.out.println("Enter the number for swapping : ");
+        Scanner scan = new  Scanner(System.in);
+        int number_to_swap=scan.nextInt();
+        System.out.println("The Swap is :"+Swap(number_to_swap));    
+    }
+
+
+}
---
+++ TwoSum.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoSum {
+
+    public static int[] findIndices(int[] arr, int t) {
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == t) {
+                return new int[]{left, right};
+            } else if (sum < t) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int[] inputArray = {1,2,4,2,1,3,7,11,15,};
+        System.out.println("Enter Target value:");
+        int targetValue = obj.nextInt();
+        int[] result = findIndices(inputArray, targetValue);
+
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No matching pair found.");
+        }
+    }
+}
