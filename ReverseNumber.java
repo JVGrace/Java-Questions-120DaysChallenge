f4d176561d1bca7ffc59ab4b6d7acf4498f33409
Pradumn Patel <pradumn.patel3101@gmail.com>
2023-06-22 19:47:47 +0530

    Second Day

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter the number:  ");  
+      int num = scan.nextInt(), reversed = 0;
+      
+      System.out.println("Original Number: " + num);
+  
+      
+      while(num != 0) {
+      
+        
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+  
+        
+        num /= 10;
+      }
+  
+      System.out.println("Reversed Number: " + reversed);
+    }
+  }
