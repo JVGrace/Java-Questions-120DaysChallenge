0bb0e7ff96fd56c5a4aebada19591a4186ce782c
Pradumn Patel <pradumn.patel3101@gmail.com>
2023-06-27 22:08:59 +0530

    Seventh Day

---
+++ BubbleSort.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++)
+                if (arr[j] > arr[j+1]) swap(arr, j, j+1);
+        }
+    }
+
+    public static void sortEarlyStop(int[] arr) {
+        boolean swapped = false;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j+1);
+                    swapped = true;
+                }
+            }
+            if (swapped == false) break;
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {10, 3, 7, 5, 1, 15, 20};
+        BubbleSort.sortEarlyStop(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        int[] arr2 = {};
+        BubbleSort.sortEarlyStop(arr2);
+        System.out.println(Arrays.toString(arr2));
+
+        int[] arr3 = {10};
+        BubbleSort.sortEarlyStop(arr3);
+        System.out.println(Arrays.toString(arr3));
+    }
+
+}
