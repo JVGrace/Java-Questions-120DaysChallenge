54d28cfd75ed041519e98a9f30780d0ec8855068
Pradumn Patel <pradumn.patel3101@gmail.com>
2023-07-05 22:43:46 +0530

    Thirteenth Day

---
+++ HourglassSolution.java
@@ -0,0 +1,51 @@
+// Java program to find maximum
+// sum of hour glass in matrix
+import java.io.*;
+
+public class Solution {
+	
+static int Row = 4;
+static int Column = 4;
+
+
+static int findMaxSum(int [][]mat)
+{
+	if (Row < 3 || Column < 3){
+		System.out.println("Not possible");
+		System.exit(0);
+	}
+
+	
+	int max_sum = Integer.MIN_VALUE;
+	for (int i = 0; i < Row - 2; i++)
+	{
+		for (int j = 0; j < Column - 2; j++)
+		{
+			
+			int sum = (mat[i][j] + mat[i][j + 1] +
+					mat[i][j + 2]) + (mat[i + 1][j + 1]) +
+					(mat[i + 2][j] + mat[i + 2][j + 1] +
+					mat[i + 2][j + 2]);
+
+			
+			max_sum = Math.max(max_sum, sum);
+		}
+	}
+	return max_sum;
+}
+
+	// Driver code
+	static public void main (String[] args)
+	{
+		int [][]mat = {{1, 2, 3, 4},
+					{5,6,7,8},
+					{9,10,1,2},
+					{3,4,5,6}};
+		int res = findMaxSum(mat);
+		System.out.println("Maximum sum of hour glass = "+ res);
+	}
+	
+}
+
+
+
---
+++ SortArray_in DescOrder_Of_Frequency.java
@@ -0,0 +1,71 @@
+// Java program to sort an array in
+// decreasing order of their frequency
+import java.util.*;
+
+class Solution{
+
+static int sortByFreq(Integer []arr, int n)
+{
+
+	                    /
+	int maxE = -1;
+
+	
+	for (int i = 0; i < n; i++) {
+		maxE = Math.max(maxE, arr[i]);
+	}
+
+	
+	int freq[] = new int[maxE + 1];
+
+	for (int i = 0; i < n; i++) {
+		freq[arr[i]]++;
+	}
+    int cnt = 0;
+
+		for (int i = 0; i <= maxE; i++) {
+
+		if (freq[i] > 0) {
+
+			int value = 100000 - i;
+			arr[cnt] = 100000 * freq[i] + value;
+			cnt++;
+		}
+	}
+
+	
+	return cnt;
+}
+
+static void printSortedArray(Integer []arr, int cnt)
+{
+
+
+	for (int i = 0; i < cnt; i++) {
+
+
+		int frequency = arr[i] / 100000;
+		int value = 100000 - (arr[i] % 100000);
+
+		for (int j = 0; j < frequency; j++) {
+			System.out.print(value + " ");
+		}
+	}
+}
+
+
+public static void main(String[] args)
+{
+
+	Integer arr[] = { 4, 4, 5, 6, 4, 2, 2, 8, 5 };
+
+	int n = arr.length;
+
+	int cnt = sortByFreq(arr, n);
+	Arrays.sort(arr, Collections.reverseOrder());
+	printSortedArray(arr, cnt);
+
+}
+}
+
+
