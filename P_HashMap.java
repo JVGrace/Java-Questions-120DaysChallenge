72fa6ef24d87ef2966bf819d45b1684c6cd8c77c
Pradumn Patel <pradumn.patel3101@gmail.com>
2023-08-12 16:02:09 +0530

    8 Day

---
+++ P_HashMap.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+
+public class P_HashMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> m = new HashMap<>();
+        m.put("First",1);
+        m.put("Second",2);
+        m.put("Third",3);
+        System.out.println(m);
+        System.out.println("Size: "+m.size());
+        System.out.println(m.containsKey("First"));
+        System.out.println(m.remove("First"));
+
+    }
+}
+/**
+ * @author Pradumn Patel */
